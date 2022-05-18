3d81fc10131d1fc6ca4981a86115a7ed5496d1f2
kimmsoll <kimmsoll@naver.com>
2022-05-18 10:47:50 +0900

    feat: pre deploy

--- src/services/disease.ts
+++ src/services/disease.ts
@@ -1,6 +1,6 @@
 import { axios } from 'hooks/worker'
 
-const url = `/B551182/diseaseInfoService/getDissNameCodeList?_type=json&sickType=1&medTp=2&numOfRows=10&ServiceKey=${process.env.REACT_APP_API_KEY}`
+const url = `https://cors-anywhere.herokuapp.com/apis.data.go.kr/B551182/diseaseInfoService/getDissNameCodeList?_type=json&sickType=1&medTp=2&numOfRows=10&ServiceKey=${process.env.REACT_APP_API_KEY}`
 
 interface Params {
   // pageNo: number
