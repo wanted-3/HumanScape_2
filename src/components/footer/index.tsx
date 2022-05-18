f583a41a30dbc5f426f75d2cb62ddb6c70267bd3
kimmsoll <kimmsoll@naver.com>
2022-05-18 19:51:39 +0900

    style: responsive CSS

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -19,18 +19,20 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className={styles.footerDetail}>
-        <div className={styles.infos}>
-          <span className={styles.footerDetailTitle}> (주)휴먼스케이프</span>
-          <span className={styles.footerLocation}>
-            서울특별시 강남구 봉은사로86길 6, 레베쌍트빌딩 601호| 대표자:장민후
-          </span>
-          <span className={styles.footerInc}> © 2021 Humanscape, All rights reserved.</span>
-          <img src='./kids.jpg' alt='' className={styles.footerIcon} />
-        </div>
-        <div className={styles.privacy}>
-          <span className={styles.footerPrivacy}> 개인정보처리방침</span>
-          <span> Living healthier by connecting better</span>
+      <div className={styles.footerDetailWrap}>
+        <div className={styles.footerDetail}>
+          <div className={styles.infos}>
+            <span className={styles.footerDetailTitle}> (주)휴먼스케이프</span>
+            <span className={styles.footerLocation}>
+              서울특별시 강남구 봉은사로86길 6, 레베쌍트빌딩 601호| 대표자:장민후
+            </span>
+            <span className={styles.footerInc}> © 2021 Humanscape, All rights reserved.</span>
+            <img src='./kids.jpg' alt='' className={styles.footerIcon} />
+          </div>
+          <div className={styles.privacy}>
+            <span className={styles.footerPrivacy}> 개인정보처리방침</span>
+            <span> Living healthier by connecting better</span>
+          </div>
         </div>
       </div>
     </footer>
